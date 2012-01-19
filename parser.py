3e7b9fa0fdbf925ed6cc723eddec2a3192d7f222
Madhusudan.C.S <madhusudancs@gmail.com>
2012-01-19 08:20:48 -0800

    Implement a node class for the parser tree.

--- parser.py
+++ parser.py
@@ -3,6 +3,56 @@ import re
 from argparse import ArgumentParser
 
 
+class Node(object):
+  """Represents a node in the parse tree.
+  """
+
+  def __init__(self, node_type=None, name=None, parent=None, children=None):
+    """Initializes a node in the parse tree along with its pointers.
+
+    Args:
+      node_type: The type of the node, can take various values, see below.
+      name: The value to be stored along with the node in the parse tree.
+      parent: The optional parent of this node in the parse tree.
+      children: The optional children of this node in the parse tree.
+
+    node_type values:
+      abstract: The node does not represent any real grammar element but an
+          abtract intermediate type in the grammar like varDecl, funcDecl. The
+          name for this type of node indicates the abstract name used.
+      keyword: The node represents the program keyword, the name stores the
+          name of the keyword.
+      ident: The node represents the ident type of data, the name stores the
+          name of the ident.
+      value: The node contains the numerical value resulting from an expression.
+          This stores the actual numerical value as the name. Since our grammar
+          supports only integer values, we always store the name of the Node
+          with type value as an integer type.
+    """
+    # convert the *args passed as tuple to list before storing it as
+    # class attributed
+    self.type = node_type
+    self.name = name
+    self.parent = parent
+    self.children = list(children) if children else []
+
+  def append_children(self, *children):
+    """Appends children to the end of the list of children.
+
+    Args:
+      children: tuple of children that must be appended to this node in order.
+    """
+
+    self.children.extend(children)
+    print self.children
+
+  def __str__(self):
+    return 'Node: %s "%s"' % (self.type, self.name)
+
+  def __repr__(self):
+    return self.__str__()
+
+
 class Parser(object):
   """Abstracts the entire grammar parser along with building the parse tree.
 
