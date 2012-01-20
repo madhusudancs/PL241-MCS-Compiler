e0c4e3b4bf8e2cb90e6b12b0eedb85faa6da213a
Madhusudan.C.S <madhusudancs@gmail.com>
2012-01-19 21:37:00 -0800

    Add the license to the parser file along with the copyright information.

--- parser.py
+++ parser.py
@@ -1,3 +1,21 @@
+# Copyright 2012 Madhusudan C.S. 
+#
+# This file parser.py is part of PL241-MCS compiler.
+#
+# PL241-MCS compiler is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PL241-MCS compiler is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PL241-MCS compiler. If not, see <http://www.gnu.org/licenses/>.
+
+
 import re
 import sys
 
