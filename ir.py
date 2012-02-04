3beba6227ae70e2ac35cea03e8ae4091a44283d3
Madhusudan.C.S <madhusudancs@gmail.com>
2012-02-04 00:11:28 -0800

    Update the IR doc string with the IR instructions.

--- ir.py
+++ ir.py
@@ -15,12 +15,38 @@
 # You should have received a copy of the GNU General Public License
 # along with PL241-MCS compiler. If not, see <http://www.gnu.org/licenses/>.
 
-"""Generates the Intermediate Representation for PL241.
+"""Generates the Intermediate Representation (IR for short) for PL241.
 
 It is on this Intermediate Representation that we perform all the machine
 independent optimizations.
+
+Allowed Intermediate Representation instructions are:
+
+    neg x                 unary minus
+    add x y               addition
+    sub x y               subtraction
+    mul x y               multiplication
+    div x y               division
+    cmp x y               comparison
+    adda x y              add two addresses x und y (used only with arrays)
+    load y                load from memory address y
+    store y x             store y to memory address x
+    move y x              assign x := y
+    phi x x1 x2 ...       x := Phi(x1, x2, x3, ...)
+    end                   end of program
+    bra y                 branch to y
+    bne x y               branch to y on x not equal
+    beq x y               branch to y on x equal
+    ble x y               branch to y on x less or equal
+    blt x y               branch to y on x less
+    bge x y               branch to y on x greater or equal
+    bgt x y               branch to y on x greater
+    read                  read
+    write                 write
+    wln                   writeLn
 """
 
+import copy
 import logging
 
 from argparse import ArgumentParser
