766a9993e0d08fcef27744d6984339ba4d7e55af
Madhusudan.C.S <madhusudancs@gmail.com>
2012-02-04 00:15:23 -0800

    Add a test case for dominance generator.

--- test_datastructures.py
+++ test_datastructures.py
@@ -1,8 +1,8 @@
 import datastructures
 
 def test_tree():
-  root = datastructures.Node(node_type='value', name=1)
-  nodes = [datastructures.Node(node_type='value', name=i) for i in range(2, 11)]
+  root = datastructures.Node(node_type='value', value=1)
+  nodes = [datastructures.Node(node_type='value', value=i) for i in range(2, 11)]
   root.append_children(nodes[0], nodes[1])
   nodes[0].append_children(nodes[2], nodes[3], nodes[4])
   nodes[1].append_children(nodes[5], nodes[6])
@@ -20,5 +20,16 @@ def test_dominator():
   nodes[6].append_out_edges(nodes[4])
   dom = datastructures.Dominator(nodes)
   dom_tree = dom.construct()
-  dom_tree.generate_dom_vcg()
+  assert set(dom_tree.dom_children) == set([nodes[1]])
+  assert set(nodes[1].dom_children) == set([nodes[2], nodes[3], nodes[7]])
+  assert set(nodes[2].dom_children) == set([])
+  assert set(nodes[3].dom_children) == set([nodes[4]])
+  assert set(nodes[4].dom_children) == set([nodes[5], nodes[6]])
+  assert set(nodes[5].dom_children) == set([])
+  assert set(nodes[6].dom_children) == set([])
+  assert set(nodes[7].dom_children) == set([])
+
+if __name__ == '__main__':
+  test_tree()
+  test_dominator()
 
