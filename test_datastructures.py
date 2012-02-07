9b2d640f6daa272f43b0d85359519d29b1a11558
Madhusudan.C.S <madhusudancs@gmail.com>
2012-02-07 02:27:51 -0800

    Add function to test dominator frontier.

--- test_datastructures.py
+++ test_datastructures.py
@@ -31,6 +31,7 @@ def test_tree():
   nodes[3].append_children(nodes[7])
   nodes[7].append_children(nodes[8])
 
+
 def test_dominator():
   nodes = [datastructures.CFGNode(i) for i in range(8)]
   nodes[0].append_out_edges(nodes[1])
@@ -51,7 +52,60 @@ def test_dominator():
   assert set(nodes[6].dom_children) == set([])
   assert set(nodes[7].dom_children) == set([])
 
+
+def test_dominance_frontier():
+  entry = datastructures.CFGNode('entry')
+  exit_node = datastructures.CFGNode('exit')
+
+  nodes = [datastructures.CFGNode(i) for i in range(0, 13)]
+
+  entry.append_out_edges(nodes[1], exit_node)
+
+  nodes[1].append_out_edges(nodes[2])
+  nodes[2].append_out_edges(nodes[3], nodes[7])
+  nodes[3].append_out_edges(nodes[4], nodes[5])
+  nodes[4].append_out_edges(nodes[6])
+  nodes[5].append_out_edges(nodes[6])
+  nodes[6].append_out_edges(nodes[8])
+  nodes[7].append_out_edges(nodes[8])
+  nodes[8].append_out_edges(nodes[9])
+  nodes[9].append_out_edges(nodes[10], nodes[11])
+  nodes[10].append_out_edges(nodes[11])
+  nodes[11].append_out_edges(nodes[9], nodes[12])
+  nodes[12].append_out_edges(nodes[2], exit_node)
+
+  dom = datastructures.Dominator([entry] + nodes[1:] + [exit_node])
+  dom_tree = dom.construct()
+  df = datastructures.DominanceFrontier(dom_tree)
+  df.compute_dominance_frontier()
+
+  assert set(dom_tree.dom_children) == set([nodes[1], exit_node])
+  assert set(nodes[1].dom_children) == set([nodes[2]])
+  assert set(nodes[2].dom_children) == set([nodes[3], nodes[7], nodes[8]])
+  assert set(nodes[3].dom_children) == set([nodes[4], nodes[5], nodes[6]])
+  assert set(nodes[8].dom_children) == set([nodes[9]])
+  assert set(nodes[9].dom_children) == set([nodes[10], nodes[11]])
+  assert set(nodes[11].dom_children) == set([nodes[12]])
+
+  assert set(entry.dominance_frontier) == set([])
+  assert set(exit_node.dominance_frontier) == set([])
+  assert set(nodes[1].dominance_frontier) == set([exit_node])
+  assert set(nodes[2].dominance_frontier) == set([exit_node, nodes[2]])
+  assert set(nodes[3].dominance_frontier) == set([nodes[8]])
+  assert set(nodes[4].dominance_frontier) == set([nodes[6]])
+  assert set(nodes[5].dominance_frontier) == set([nodes[6]])
+  assert set(nodes[6].dominance_frontier) == set([nodes[8]])
+  assert set(nodes[7].dominance_frontier) == set([nodes[8]])
+  assert set(nodes[8].dominance_frontier) == set([exit_node, nodes[2]])
+  assert set(nodes[9].dominance_frontier) == set([exit_node, nodes[2],
+                                                  nodes[9]])
+  assert set(nodes[10].dominance_frontier) == set([nodes[11]])
+  assert set(nodes[11].dominance_frontier) == set([exit_node, nodes[2],
+                                                   nodes[9]])
+  assert set(nodes[12].dominance_frontier) == set([exit_node, nodes[2]])
+
+
 if __name__ == '__main__':
   test_tree()
   test_dominator()
-
+  test_dominance_frontier()
