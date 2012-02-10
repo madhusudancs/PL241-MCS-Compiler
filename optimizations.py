6e4f58667eca78ed7237e721f9488beb4e7e4cf0
Madhusudan.C.S <madhusudancs@gmail.com>
2012-02-10 09:35:41 -0800

    Add a file to store all the optimizations we perform, just a basic file with bootstrap.

---
+++ optimizations.py
@@ -0,0 +1,86 @@
+# Copyright 2012 Madhusudan C.S. 
+#
+# This file optimizations.py is part of PL241-MCS compiler.
+#
+# PL241-MCS compiler is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PL241-MCS compiler is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PL241-MCS compiler. If not, see <http://www.gnu.org/licenses/>.
+
+"""Algorithms to perform optimizations on the input source program.
+"""
+
+
+from argparse import ArgumentParser
+
+from ir import IntermediateRepresentation
+from parser import LanguageSyntaxError
+from parser import Parser
+
+
+def cse_cp(ssa):
+  """Performs common sub-expression elimination and copy propogation.
+
+  Args:
+    ssa: The SSA object.
+  """
+  # For each statement stores its replacement for either the statement
+  # itself or the result of the statement if the statement was removed because
+  # of Common Sub-expression elimination or Copy propogation.
+  replacements = {}
+  for instruction in ssa.ssa:
+    pass
+
+
+def bootstrap():
+  parser = ArgumentParser(description='Compiler arguments.')
+  parser.add_argument('file_names', metavar='File Names', type=str, nargs='+',
+                      help='name of the input files.')
+  parser.add_argument('-d', '--debug', action='store_true',
+                      help='Enable debug logging to the console.')
+  parser.add_argument('-g', '--vcg', metavar="VCG", type=str,
+                      nargs='?', const=True,
+                      help='Generate the Visualization Compiler Graph output.')
+  args = parser.parse_args()
+
+  if args.debug:
+    LOGGER.setLevel(logging.DEBUG)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+    LOGGER.addHandler(ch)
+
+  try:
+    p = Parser(args.file_names[0])
+    ir = IntermediateRepresentation(p)
+
+    ir.generate()
+    cfg = ir.build_cfg()
+    cfg.compute_dominance_frontiers()
+
+    ssa = SSA(ir, cfg)
+    ssa.construct()
+
+    optimized = cse_cp(ssa)
+
+    if args.vcg:
+      vcg_file = open(args.vcg, 'w') if isinstance(args.vcg, str) else \
+          sys.stdout
+      vcg_file.write(p.root.generate_vcg())
+      vcg_file.close()
+
+    return p.root
+
+  except LanguageSyntaxError, e:
+    print e
+    sys.exit(1)
+
+if __name__ == '__main__':
+  root = bootstrap()
