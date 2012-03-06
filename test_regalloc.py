72d1c4c354cd28f60cb1a459b0964e7683ac85ed
Madhusudan.C.S <madhusudancs@gmail.com>
2012-03-06 05:15:33 -0800

    Rewrite the test cases completely for the new SAT solver architecture.

--- test_regalloc.py
+++ test_regalloc.py
@@ -20,87 +20,70 @@
 
 import math
 import datastructures
+import regalloc
 import subprocess
 
 
 def test_bool_sat():
   """Tests if the boolean satisfiability is working as expected.
   """
-  n1 = datastructures.InterferenceNode('r1', (10, 20))
-  n2 = datastructures.InterferenceNode('r2', (15, 25))
-  n3 = datastructures.InterferenceNode('r3', (18, 30))
-  n4 = datastructures.InterferenceNode('r4', (28, 40))
+  r1 = regalloc.Register()
+  r2 = regalloc.Register()
+  r3 = regalloc.Register()
+  r4 = regalloc.Register()
+
+  n1 = datastructures.InterferenceNode(r1, (10, 20))
+  n2 = datastructures.InterferenceNode(r2, (15, 25))
+  n3 = datastructures.InterferenceNode(r3, (18, 30))
+  n4 = datastructures.InterferenceNode(r4, (28, 40))
 
   n1.append_edges(n2, n3)
   n2.append_edges(n3)
 
-  registers = 2
+  ifg = datastructures.InterferenceGraph([n1, n2, n3, n4])
+  ra = regalloc.RegisterAllocator(None, 4)
+  is_solved, allocation = ra.sat_solve(ifg)
+  assert is_solved
+  print "Test Case 1 Passed! Allocation: %s" % (allocation)
+
+  r5 = regalloc.Register()
+
+  n1 = datastructures.InterferenceNode(r1, (10, 20))
+  n2 = datastructures.InterferenceNode(r2, (15, 25))
+  n3 = datastructures.InterferenceNode(r3, (18, 30))
+  n4 = datastructures.InterferenceNode(r4, (19, 40))
+  n5 = datastructures.InterferenceNode(r5, (14, 18))
+
+  n1.append_edges(n2, n3, n4, n5)
+  n2.append_edges(n3, n4, n5)
+  n3.append_edges(n4)
 
   ifg = datastructures.InterferenceGraph([n1, n2, n3, n4])
+  ra = regalloc.RegisterAllocator(None, 4)
+  is_solved, allocation = ra.sat_solve(ifg)
+  assert is_solved
+  print 'Test Case 2 Passed! Allocation: %s' % (allocation)
 
-  # Dictionary holding the 
-  processed = {}
+  r6 = regalloc.Register()
 
-  conflicting_registers = {}
+  n1 = datastructures.InterferenceNode(r1, (10, 20))
+  n2 = datastructures.InterferenceNode(r2, (15, 25))
+  n3 = datastructures.InterferenceNode(r3, (18, 30))
+  n4 = datastructures.InterferenceNode(r4, (19, 40))
+  n5 = datastructures.InterferenceNode(r5, (14, 18))
+  n6 = datastructures.InterferenceNode(r6, (18, 20))
 
-  last_pattern = bin(registers - 1)[2:]
-  last_pattern_length = len(last_pattern)
+  n1.append_edges(n2, n3, n4, n5, n6)
+  n2.append_edges(n3, n4, n5, n6)
+  n3.append_edges(n4, n6)
+  n4.append_edges(n6)
 
-  bit_template = [last_pattern]
-  for i in range(registers - 2, -1, -1):
-    pattern = ('%s' % bin(i)[2:]).zfill(last_pattern_length)
-    bit_template.append(pattern)
+  ifg = datastructures.InterferenceGraph([n1, n2, n3, n4])
+  ra = regalloc.RegisterAllocator(None, 4)
+  is_solved, allocation = ra.sat_solve(ifg)
+  assert not is_solved
+  print 'Test Case 3 Passed! Allocation failed'
 
-  clauses = []
-
-  for node in ifg:
-    node_reg = node.register[1:]
-    if node.edges:
-      conflicting_registers[node_reg] = True
-
-    for edge in node.edges:
-      edge_reg = edge.register[1:]
-      if ((edge_reg, node_reg) in processed) or (
-          (node_reg, edge_reg) in processed):
-        continue
-
-      processed[(node_reg, edge_reg)] = True
-
-      conflicting_registers[edge_reg] = True
-
-      for template in bit_template:
-        clause = ''
-        for bit_position, bit in enumerate(template):
-          if bit == '0':
-            clause += ('-%(node_reg)s%(bit_position)d '
-                '-%(edge_reg)s%(bit_position)d ') % {
-                    'node_reg': node_reg,
-                    'edge_reg': edge_reg,
-                    'bit_position': bit_position,
-                }
-          else:
-            clause += ('%(node_reg)s%(bit_position)d '
-                '%(edge_reg)s%(bit_position)d ') % {
-                    'node_reg': node_reg,
-                    'edge_reg': edge_reg,
-                    'bit_position': bit_position,
-                    }
-        clauses.append(clause)
-
-  cnf = 'p cnf %d %d\n%s0' % (
-       len(conflicting_registers) * math.ceil(math.log(registers)),
-       len(clauses),
-       '0\n'.join(clauses))
-
-  process = subprocess.Popen('glucose_static', stdin=subprocess.PIPE,
-                             stdout=subprocess.PIPE)
-  output = process.communicate(cnf)
-  # Read last 4 lines
-  lines = output.rsplit('\n', 4)
-  if lines[0] == 's UNSATISFIABLE':
-    print "Not Satisfiable"
-  elif lines[1] == 's SATISFIABLE':
-    print "Allocation: %s"
 
 if __name__ == '__main__':
   test_output = test_bool_sat()
