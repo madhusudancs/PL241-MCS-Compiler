ec25e5e959672ddb83ccd0cb3bfa57dbde5f3cc4
Madhusudan.C.S <madhusudancs@gmail.com>
2012-03-07 02:46:00 -0800

    Fix a major bug in SSA where the phi instruction assignment counts were not popped when backtracking the dominator tree.

--- ssa.py
+++ ssa.py
@@ -208,6 +208,8 @@ class SSA(object):
       if instruction.instruction == 'move' and hasattr(
           instruction, 'old_operand2'):
         stacks[instruction.old_operand2].pop()
+      for phi_variable in root.phi_functions:
+        stacks[phi_variable].pop()
 
   def rename(self):
     """Rename all the variables for SSA representation.
