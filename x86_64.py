3844b735881814ec74cf8d69064cdd10f750e399
Madhusudan.C.S <madhusudancs@gmail.com>
2012-05-31 21:58:37 -0700

    Use special encoding for IMUL instruction's 3 address addressing mode.

--- x86_64.py
+++ x86_64.py
@@ -676,15 +676,10 @@ class IMUL(Instruction):
     """
     super(IMUL, self).__init__(destination, source, immediate=immediate)
 
-  def build(self):
-    """Builds the instruction bytes.
-
-    IMPORTANT: The only difference between this method and the Instruction
-    class's build method is that the instruction opcode is encoded into
-    2 bytes not a single byte.
+  def reg64_rm64_imm32(self):
+    """Builds the instruction bytes for reg64, rm64, imm32 operands.
     """
-    self.binary = ''
-    if self.immediate != None:
+    if isinstance(self.source, Register):
       source_reg = REGISTER_COLOR_TO_CODE_MAP[self.source.color]
       dest_reg = REGISTER_COLOR_TO_CODE_MAP[self.destination.color]
       imm32 = self.immediate.value
@@ -709,6 +704,64 @@ class IMUL(Instruction):
                                  modregrm)
       self.binary += struct.pack('%si' % BYTE_ORDERING_FMT,
                                  imm32)
+
+    elif isinstance(self.source, Memory):
+      source_mem = self.source
+      dest_reg = REGISTER_COLOR_TO_CODE_MAP[self.destination.color]
+      imm32 = self.immediate.value
+
+      opcode_entry = self.OPCODE_TABLE[('reg64', 'rm64', 'imm32')]
+
+      mod = 0b10
+      reg = dest_reg['REG']
+
+      if isinstance(source_mem.base, Register):
+        source_reg = REGISTER_COLOR_TO_CODE_MAP[source_mem.base.color]
+        rm = source_reg['REG']
+        offset = self.displacement if self.displacement else source_mem.offset
+      elif source_mem.base == 'rip':
+        # mod is 00 for RIP-relative addressing
+        mod = 0b00
+        # For %rip register
+        source_reg = REGISTER_COLOR_TO_CODE_MAP['rip']
+        rm = source_reg['REG']
+        offset = self.displacement if self.displacement else source_mem.offset
+      else:
+        source_reg = REGISTER_COLOR_TO_CODE_MAP['rbp']
+        rm = source_reg['REG']
+        offset = -source_mem.offset if source_mem.offset else \
+            (source_mem.offset if source_mem.offset else 0) * MEMORY_WIDTH
+
+      modregrm = self.mod_reg_rm_byte(mod, reg, rm)
+      rex = self.rex_byte(base=opcode_entry['REX'],
+                          R=dest_reg['REX'],
+                          B=source_reg['REX'])
+
+      if rex:
+        self.binary += struct.pack('%sB' % BYTE_ORDERING_FMT, rex)
+
+      # Opcode entries are properly byte ordered, so preserve the order
+      # using big-endian
+      self.binary += struct.pack('>B', opcode_entry['OPCODE'])
+      self.binary += struct.pack('%sB' % BYTE_ORDERING_FMT,
+                                 modregrm)
+      self.binary += struct.pack('%si' % BYTE_ORDERING_FMT, offset)
+      self.binary += struct.pack('%si' % BYTE_ORDERING_FMT,
+                                 imm32)
+
+    else:
+      raise NotImplementedError('Instruction could not be encoded.')
+
+  def build(self):
+    """Builds the instruction bytes.
+
+    IMPORTANT: The only difference between this method and the Instruction
+    class's build method is that the instruction opcode is encoded into
+    2 bytes not a single byte.
+    """
+    self.binary = ''
+    if self.immediate != None:
+      self.reg64_rm64_imm32()
     elif (isinstance(self.destination, Register) and
         (isinstance(self.source, Register))):
       source_reg = REGISTER_COLOR_TO_CODE_MAP[self.source.color]
