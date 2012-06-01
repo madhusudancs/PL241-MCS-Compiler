e98497902c1b0c7efb858d79b8775137f393eb32
Madhusudan.C.S <madhusudancs@gmail.com>
2012-06-01 03:28:11 -0700

    Move the SUB instruction to keep it in alphabetical order.

--- x86_64.py
+++ x86_64.py
@@ -1175,6 +1175,22 @@ class RET(Instruction):
     self.binary += struct.pack('>B', self.OPCODE_TABLE['nooperand']['OPCODE'])
 
 
+class SUB(Instruction):
+  """Implements the SUB instruction.
+  """
+
+  OPCODE_TABLE = {
+      ('reg64', 'rm64'): { 'REX': 0x48, 'OPCODE': 0x2B },
+      ('rm64', 'reg64'): { 'REX': 0x48, 'OPCODE': 0x29 },
+      ('rm64', 'imm32'): { 'REX': 0x48, 'OPCODE': 0x81, 'OPCODE_EXT': 0x5 }
+      }
+
+  def __init__(self, destination, source):
+    """Constructs the SUB instruction.
+    """
+    super(SUB, self).__init__(destination, source)
+
+
 class SYSCALL(Instruction):
   """Implements the SYSCALL instruction.
   """
@@ -1196,22 +1212,6 @@ class SYSCALL(Instruction):
     # Opcode entries are properly byte ordered, so preserve the order
     # using big-endian
     self.binary += struct.pack('>H', self.OPCODE_TABLE['nooperand']['OPCODE'])
-
-
-class SUB(Instruction):
-  """Implements the SUB instruction.
-  """
-
-  OPCODE_TABLE = {
-      ('reg64', 'rm64'): { 'REX': 0x48, 'OPCODE': 0x2B },
-      ('rm64', 'reg64'): { 'REX': 0x48, 'OPCODE': 0x29 },
-      ('rm64', 'imm32'): { 'REX': 0x48, 'OPCODE': 0x81, 'OPCODE_EXT': 0x5 }
-      }
-
-  def __init__(self, destination, source):
-    """Constructs the SUB instruction.
-    """
-    super(SUB, self).__init__(destination, source)
 
 
 class XCHG(Instruction):
